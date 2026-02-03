e541938a5b16f242a188b4d4df47c3b151fadb2d
czf <5413699@qq.com>
2026-02-03 21:21:04 +0800

    动态数组的删除-初始化

---
+++ src/main/java/com/czf/arraylist/DynamicArray.java
@@ -0,0 +1,129 @@
+package com.czf.arraylist;
+
+import java.util.Arrays;
+import java.util.Iterator;
+import java.util.function.Consumer;
+import java.util.stream.IntStream;
+
+
+public class DynamicArray implements Iterable<Integer> {
+    private int size = 0; // 逻辑大小
+    private int capacity = 8; // 容量
+    private int[] array = {};
+
+    public int[] array() {
+        return Arrays.copyOf(array, size);
+    }
+
+    /**
+     * 向最后位置 [size] 添加元素
+     *
+     * @param element 待添加元素
+     */
+    public void addLast(int element) {
+//        array[size] = element;
+//        size++;
+        add(size, element);
+    }
+
+    /**
+     * 向 [0 .. size] 位置添加元素
+     *
+     * @param index   索引位置
+     * @param element 待添加元素
+     */
+    public void add(int index, int element) {
+        checkAndGrow();
+
+        // 添加逻辑
+        if (index >= 0 && index < size) {
+            // 向后挪动, 空出待插入位置
+            System.arraycopy(array, index,
+                    array, index + 1, size - index);
+        }
+        array[index] = element;
+        size++;
+    }
+
+    private void checkAndGrow() {
+        // 容量检查
+        if (size == 0) {
+            array = new int[capacity];
+        } else if (size == capacity) {
+            // 进行扩容, 1.5 1.618 2
+            capacity += capacity >> 1;
+            int[] newArray = new int[capacity];
+            System.arraycopy(array, 0,
+                    newArray, 0, size);
+            array = newArray;
+        }
+    }
+
+
+
+
+    /**
+     * 查询元素
+     *
+     * @param index 索引位置, 在 [0..size) 区间内
+     * @return 该索引位置的元素
+     */
+    public int get(int index) {
+        return array[index];
+    }
+
+    /**
+     * 遍历方法1
+     *
+     * @param consumer 遍历要执行的操作, 入参: 每个元素
+     */
+    public void foreach(Consumer<Integer> consumer) {
+        for (int i = 0; i < size; i++) {
+            // 提供 array[i]
+            // 返回 void
+            consumer.accept(array[i]);
+        }
+    }
+
+
+    @Override
+    public Iterator<Integer> iterator() {
+        /**
+         * 根据接口的抽象方法签名可以看出，我们需要返回一个Iterator<Integer>类型的变量，
+         * 因此我们创建对象返回。（匿名内部类）
+         * Iterator有两个需要实现的方法：
+         * 1. boolean hasNext()：判断是否还有下一个元素。
+         * 2. E next()：返回下一个元素。
+         */
+
+        return new Iterator<Integer>() {
+            // i代表当前索引的位置，从数组头部开始遍历
+            int i=0;
+            @Override
+            public boolean hasNext() {//判断是否还有下一个元素。
+                // 判断当前索引是否小于数组长度
+                return i<size;
+            }
+
+            // 返回当前元素的位置
+            @Override
+            public Integer next() {//先返回当前元素。再移动到下一个元素。
+                return array[i++];
+            }
+        };
+    }
+
+    /**
+     * 遍历方法3 - stream 遍历
+     *
+     * @return stream 流
+     */
+    public IntStream stream(){
+        // .of方法可以将数组转换成stream流
+        // 但是不能将array当成数组传给of方法
+        // 因为这样数组的有效部分还是无效部分都会被遍历
+        // 所以需要使用Arrays.copyOfRange方法, 将array的有效部分转换成stream流
+        // 注意这里的区间是含头不含尾的
+        return IntStream.of(Arrays.copyOfRange(array, 0, size));
+    }
+}
---
+++ src/test/java/com/czf/arraylist/TestDynamicArray.java
@@ -0,0 +1,64 @@
+package com.czf.arraylist;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.function.Consumer;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class TestDynamicArray {
+
+    @Test
+    @DisplayName("测试添加")
+    public void test1() {
+        DynamicArray dynamicArray = new DynamicArray();
+        dynamicArray.addLast(1);
+        dynamicArray.addLast(2);
+        dynamicArray.addLast(3);
+        dynamicArray.addLast(4);
+
+        dynamicArray.forEach((element)-> {
+            System.out.println(element);
+        });
+    }
+
+    @Test
+    @DisplayName("测试使用迭代器进行遍历")
+    public void test2() {
+        DynamicArray dynamicArray = new DynamicArray();
+        dynamicArray.addLast(1);
+        dynamicArray.addLast(2);
+        dynamicArray.addLast(3);
+        dynamicArray.addLast(4);
+
+        // 使用增强for循环进行遍历
+        // 增强for循环的内部就是
+        // 在每次循环时调用迭代器的hasnext方法看看有没有下一个元素，如果有，就继续循环
+        // 每次循环时调用next方法，将获取到的当前元素赋值给element，并移动指针
+        //！因此如果没有实现迭代器，增强for循环就无法使用。
+        for (Integer element : dynamicArray) {
+            System.out.println(element);
+        }
+
+    }
+
+    @Test
+    @DisplayName("测试使用流的方式进行遍历")
+    public void test3() {
+        DynamicArray dynamicArray = new DynamicArray();
+        dynamicArray.addLast(1);
+        dynamicArray.addLast(2);
+        dynamicArray.addLast(3);
+        dynamicArray.addLast(4);
+
+        //既然是流了，我们就可以调用stream里的一些方法，比如forEach
+        //修改后，会发现此次仅遍历了有效的1234，成功
+        dynamicArray.stream().forEach(element -> {
+            System.out.println(element);
+        });
+    }
+
+}
