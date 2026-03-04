0876008661bc0b095908ce26c8a0d83f794e0972
czf <5413699@qq.com>
2026-03-04 13:30:37 +0800

    数组Leetcode习题：M053. 最大子数组和 动态规划解法

--- src/main/java/com/czf/arraylist/M053_Maximum_SubarraySum.java
+++ src/main/java/com/czf/arraylist/M053_Maximum_SubarraySum.java
@@ -16,18 +16,8 @@ public class M053_Maximum_SubarraySum {
 
     public static void main(String[] args) {
         M053_Maximum_SubarraySum solution = new M053_Maximum_SubarraySum();
-            
-        // 示例 1
-        System.out.println("示例 1：-2,1,-3,4,-1,2,1,-5,4");
-        int[] nums = new int[]{-2,1,-3,4,-1,2,1,-5,4};
-        System.out.println("结果：" + solution.maxSubArray_brute(nums));
-            
-        // 示例 2
-        System.out.println("\n示例 2：-1");
-        int[] nums2 = new int[]{-1};
-        System.out.println("结果：" + solution.maxSubArray_brute(nums2));
-            
-        // 示例 3: ACM 模式 - 从控制台读取输入
+
+        // ACM 模式 - 从控制台读取输入
         System.out.println("\n=== ACM 模式 ===");
         System.out.println("请输入数组长度：");
         Scanner sc = new Scanner(System.in);
@@ -41,7 +31,9 @@ public class M053_Maximum_SubarraySum {
             
         int result = solution.maxSubArray_brute(nums3);
         System.out.println("最大子数组和：" + result);
-            
+        int result2 = solution.maxSubArray(nums3);
+        System.out.println("最大子数组和：" + result2);
+
         sc.close();
     }
 
@@ -62,7 +54,24 @@ public class M053_Maximum_SubarraySum {
         }
         return max;
     }
+    /**
+     * 动态规划思路
+     */
 
-
+    public int maxSubArray(int[] nums) {
+        int max =  nums[0];  // 用max记录整个数组的最大子数组和，这里用Min_VALUE是为了避免数组全是负数
+        int dp = nums[0];   //记录以当前元素结尾的，最大子数组和
+        // 遍历每个元素，计算以该元素结尾的最大子数组
+        for (int i = 1; i < nums.length; i++) {
+            // 假如每次只添加一个元素，
+            // 以该元素结尾的最大子数组和
+            // 要么是以上一个元素结尾的数组的最大子数组和(>0时)加上该元素
+            // 要么是该元素本身【以上一个元素结尾的数组的最大子数组和(<0时)】
+            dp = Math.max(dp+nums[i],nums[i]);
+            // 找出最大的子数组和
+            max = Math.max(max,dp);
+        }
+        return max;
+    }
 
 }
