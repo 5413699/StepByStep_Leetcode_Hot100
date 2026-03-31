fd4bd777ed937f7319ff7fe7c5029e1fd2004cf2
czf <5413699@qq.com>
2026-03-31 15:01:56 +0800

    数组Leetcode习题：M238. 除了自身以外数组的乘积 前缀乘积 + 后缀乘积法 先正序计算左前缀乘积存数组，用变量R表示初始的右后缀乘积， 计算完左前缀后，用最后一个左前缀*R得到初始的数组乘积 再更新R用于为下一轮循环提供正确的右边乘积，迭代进行即可

--- src/main/java/com/czf/arraylist/M238_Product_Expect_Itself.java
+++ src/main/java/com/czf/arraylist/M238_Product_Expect_Itself.java
@@ -20,12 +20,40 @@ public class M238_Product_Expect_Itself {
         }
 
     }
+
+    /**
+     *先正序计算左前缀乘积存数组，用变量R表示初始的右后缀乘积，
+     * 计算完左前缀后，用最后一个左前缀*R得到初始的数组乘积
+     * 再更新R用于为下一轮循环提供正确的右边乘积，迭代进行即可
+     */
+    public static int[] productExceptSelf(int[] nums) {
+        int length = nums.length;
+        // 存答案/前缀乘积
+        int[] ans = new int[length];
+        // 存后缀乘积
+        int r = 1;
+        // 第一个元素左边没有元素
+        ans[0] = 1;
+        // 计算左前缀乘积
+        for(int i = 1; i < length; i++){
+            ans[i] = ans[i-1] * nums[i-1];
+        }
+        // 从后往前计算后缀乘积（最右侧元素的数组乘积等于其左前缀乘以该最右侧元素）
+        for(int j = length-1 ; j >= 0; j--){
+            // 计算数组乘积
+            ans[j] = ans[j] * r;
+            // 更新后缀乘积
+            r = r * nums[j];
+        }
+        return ans;
+    }
+
     /**
      * 常规方法，时间复杂度高
      * @param nums
      * @return
      */
-    public static int[] productExceptSelf(int[] nums) {
+    public static int[] productExceptSelf1(int[] nums) {
         int length = nums.length;
         int[] ans = new int[length];
         // 计算第i个元素的乘积
