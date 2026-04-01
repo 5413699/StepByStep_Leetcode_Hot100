06e3b762d4ee1b26a485315056d0d0b6d5a58bc0
czf <5413699@qq.com>
2026-04-01 17:24:31 +0800

    数组Leetcode习题：E141 环形链表 快慢指针（Floyd 判圈算法） 如果链表有环，快指针每次走两步，慢指针走一步，最终快指针会在环中追上慢指针。 如果链表无环，快指针会先到达 null，结束循环。
    注意：
    1.快指针一次动2步，因此需要对fast.next进行判空
    while(fast != null && fast.next != null)
    2.先移动，后判断，否则初始都在一个位置，会导致循环停止

---
+++ src/main/java/com/czf/linkedlist/E141_Cycle_Linkedlist.java
@@ -0,0 +1,55 @@
+package com.czf.linkedlist;
+
+import java.util.HashMap;
+
+/**
+ * ClassName: E141_Cycle_Linkedlist
+ * Package: com.czf.linkedlist
+ * Description:
+ *
+ * @Author 陈智飞
+ * @Create 2026/4/1 16:59
+ * @Version 1.0
+ */
+public class E141_Cycle_Linkedlist {
+    private static class ListNode{
+        int val;
+        ListNode next;
+        ListNode(int val){
+            this.val = val;
+        }
+        ListNode(int val, ListNode next){
+            this.val = val;
+            this.next = next;
+        }
+    }
+
+    public static void main(String[] args) {
+        ListNode head = new ListNode(3);
+        ListNode head1 = new ListNode(2);
+        ListNode head2 = new ListNode(0);
+        ListNode head3 = new ListNode(-4);
+        head.next = head1;
+        head1.next = head2;
+        head2.next = head3;
+        head3.next = head1;
+
+        boolean ans = hasCycle(head);
+        System.out.println(ans);
+    }
+    public static boolean hasCycle(ListNode head) {
+        ListNode fast = head;
+        ListNode slow = head;
+
+        while(fast != null && fast.next != null){
+            fast = fast.next.next;
+            slow = slow.next;
+            if(fast == slow){
+                return true;
+            }
+
+        }
+
+        return false;
+    }
+}
