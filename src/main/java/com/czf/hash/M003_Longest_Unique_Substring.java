9b2072523ba682cf036712c771fea6e7c66c5522
czf <5413699@qq.com>
2026-04-09 22:08:19 +0800

    滑动窗口Leetcode习题：M003. 无重复字符的最长子串 简易版 1.首先处理空字符串的情况，若字符串为空或有效长度为0，直接返回0 2.使用hashset存储字串数据，以快速判断字符是否存在于子串中 3.若输入非空，令左右指针指向字符串第一个元素，进行滑动窗口搜索 3.1 添加右指针元素时，如果待添加元素不在集合中，将右指针元素加入set中，如果当前字串长度超过前面计算的最大值，进行更新。 3.2 如果待添加元素在集合中,重复了，删除左侧指针元素，左侧指针向右移动 错误点 1.在 Java 中，泛型（例如 Set<>）不支持基本数据类型 char。你需要使用它的包装类 Character。 2.可以先处理空字符串的情况。初始化滑动窗口指针建议左右都设为0，在循环中添加右指针元素即可，避免空字符串带来报错， 3.可直接用right-left记录当前子串长度， 4.只要**右指针（负责扩张的那个）**没走到字符串末尾，我们就要继续探索

---
+++ src/main/java/com/czf/hash/M003_Longest_Unique_Substring.java
@@ -0,0 +1,56 @@
+package com.czf.hash;
+
+import java.util.HashSet;
+import java.util.Set;
+
+/**
+ * ClassName: M003_Longest_Unique_Substring
+ * Package: com.czf.hash
+ * Description:
+ *
+ * @Author 陈智飞
+ * @Create 2026/4/9 22:01
+ * @Version 1.0
+ */
+public class M003_Longest_Unique_Substring {
+    public static void main(String[] args) {
+        String s ="abcacbb";
+        int ans = lengthOfLongestSubstring1(s);
+        System.out.println(ans);
+    }
+    public static int lengthOfLongestSubstring1(String s) {
+        // 记录答案
+        int ans = 0;
+        // 字符串长度
+        int length = s.length();
+        // 滑动窗口指针
+        int left = 0;
+        int right = 0;
+
+        // 处理空字符串的情况
+        if(s==null || s.length()== 0){
+            return 0;
+        }
+        // 初始化当前子串
+        Set<Character> charSet = new HashSet<Character>();
+
+        // 若输入非空，对窗口进行滑动
+        while(right < length){
+            // 添加右指针元素时，如果待添加元素不在集合中
+            if(!charSet.contains(s.charAt(right))){
+                // 将右指针元素加入set中
+                charSet.add(s.charAt(right));
+                right = right + 1;
+                // 如果当前字串长度超过前面计算的最大值，进行更新
+                ans = Math.max(right-left, ans);
+            }else{// 如果待添加元素在集合中,重复了
+                // 删除左侧指针元素
+                charSet.remove(s.charAt(left));
+                // 左侧指针向右移动
+                left = left + 1;
+            }
+
+        }
+        return ans;
+    }
+}
