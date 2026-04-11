1c61fdd0262818520ff2379346a73e05e7f9f5b0
czf <5413699@qq.com>
2026-04-11 15:31:29 +0800

    滑动窗口Leetcode习题：M003. 无重复字符的最长子串 解法2： 使用int[],根据ascii码分配索引以快速判断字符是否在字串中 其他逻辑大体相同

--- src/main/java/com/czf/hash/M003_Longest_Unique_Substring.java
+++ src/main/java/com/czf/hash/M003_Longest_Unique_Substring.java
@@ -1,5 +1,6 @@
 package com.czf.hash;
 
+import java.util.Arrays;
 import java.util.HashSet;
 import java.util.Set;
 
@@ -15,9 +16,46 @@ import java.util.Set;
 public class M003_Longest_Unique_Substring {
     public static void main(String[] args) {
         String s ="abcacbb";
-        int ans = lengthOfLongestSubstring1(s);
+        int ans = lengthOfLongestSubstring(s);
         System.out.println(ans);
     }
+
+    public static int lengthOfLongestSubstring(String s) {
+        // 记录答案
+        int ans = 0;
+        // 字符串长度
+        int length = s.length();
+        // 滑动窗口指针
+        int left = 0;
+        int right = 0;
+
+        // 初始化最后一次出现的索引位置
+        int[] lastSeen = new int[128];
+        Arrays.fill(lastSeen, -1);
+
+        // 处理空字符串的情况
+        if(s==null || s.length()== 0){
+            return 0;
+        }
+
+
+        // 若输入非空，对窗口进行滑动
+        while(right < length){
+            // 字符对应的asxii码，对应数组索引
+            int index = s.charAt(right);
+            // 假如滑动窗口内出现了当前子串中的字符
+            // 更新左指针的位置
+            left = Math.max(left, lastSeen[index] + 1);
+            // 更新当前字符在lastseen中的位置
+            lastSeen[index] = right;
+            // 计算字串长度
+            ans = Math.max(right - left + 1 , ans);
+            // 移动右指针
+            right = right + 1;
+        }
+        return ans;
+    }
+
     public static int lengthOfLongestSubstring1(String s) {
         // 记录答案
         int ans = 0;
