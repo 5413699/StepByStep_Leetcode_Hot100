74df84954e0e4af3a43eb680c4b33427982a55f8
czf <5413699@qq.com>
2026-04-23 16:50:10 +0800

    链表 Leetcode习题：M024-两两交换链表中的节点 1.新建虚拟头节点用于处理操作第一个节点的情况，初始化temp记录当前链表处理到了哪个节点 2.确保链表中还有两个节点可供交换位置后，从链表中不断取出两个节点进行操作dump - 1 - 2 - 3 要交换成 dump - 2 - 1 - 3 首先要将节点1，节点2表示出来 3.执行指针的交换流程   dump指向2,节点1指向3，节点2指向1 4.更新temp指针使其指向1节点 5.返回dummy.next

---
+++ src/main/java/com/czf/linkedlist/M024_Swap_Node_InPairs.java
@@ -0,0 +1,89 @@
+package com.czf.linkedlist;
+
+/**
+ * ClassName: M024_Swap_Node_InPairs
+ * Package: com.czf.linkedlist
+ * Description:
+ *
+ * @Author 陈智飞
+ * @Create 2026/4/23 15:09
+ * @Version 1.0
+ */
+public class M024_Swap_Node_InPairs {
+
+
+
+    public static class ListNode{
+        int val;
+        ListNode next;
+        ListNode(){};
+        ListNode(int val){
+            this.val = val;
+        }
+        ListNode(int val, ListNode next){
+            this.val = val;
+            this.next = next;
+        }
+        @Override
+        public String toString(){
+            StringBuilder sb = new StringBuilder("[");
+            ListNode cur = this;
+            while (cur != null){
+                sb.append(cur.val);
+                if(cur.next != null){
+                    sb.append(",");
+                }
+                cur = cur.next;
+            }
+            sb.append("]");
+            return sb.toString();
+        }
+
+    }
+
+
+    public static void main(String[] args) {
+        ListNode head = new ListNode(1,
+                new ListNode(2,
+                        new ListNode(3,
+                                new ListNode(4))));
+        ListNode ans = swapPairs(head);
+        System.out.println(ans);
+    }
+
+
+
+    /**
+     * 迭代方法
+     *
+     */
+    public static ListNode swapPairs(ListNode head) {
+        // 因为要对链表的头结点进行修改，创建虚拟头结点进行操作
+        ListNode dummy = new ListNode(0);
+        dummy.next = head;
+        // 记录当前链表处理到了哪个节点
+        ListNode temp = dummy;
+
+        // 从链表中不断取出两个节点进行操作
+        // 确保链表中还有两个节点可供交换位置
+        while(temp.next != null && temp.next.next != null){
+            //          dump - 1 - 2 - 3
+            // 要交换成 dump - 2 - 1 - 3
+            //          temp
+            // 首先要将节点1，节点2表示出来
+            ListNode temp1 = temp.next;
+            ListNode temp2 = temp1.next;
+            // 执行指针的交换流程
+            // dump指向2,节点1指向3，节点2指向1
+            temp.next = temp2;
+            temp1.next = temp2.next;
+            temp2.next = temp1;
+            // 更新temp指针使其指向1节点
+            temp = temp1;
+        }
+
+        return dummy.next;
+    }
+
+
+}
