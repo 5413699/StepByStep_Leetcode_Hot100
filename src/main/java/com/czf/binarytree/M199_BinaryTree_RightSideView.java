1dfa7a289f49b72344d3fc34f5f09c1942be005d
czf <5413699@qq.com>
2026-05-21 15:30:23 +0800

    199. 二叉树的右视图 右视图的本质是每一层的最后一个节点， 所以可以用队列层序遍历二叉树。 每次进入一层时，先记录当前队列大小 size代表当前层节点数； 然后遍历这一层的所有节点， 当 i == size - 1 时，说明当前节点是这一层从左到右遍历中的最后一个节点， 也就是右视图能看到的节点，将它加入答案。 遍历过程中继续把左右孩子加入队列。最终返回答案列表。

--- src/main/java/com/czf/binarytree/M199_BinaryTree_RightSideView.java
+++ src/main/java/com/czf/binarytree/M199_BinaryTree_RightSideView.java
@@ -1,6 +1,9 @@
 package com.czf.binarytree;
 
+import java.util.ArrayList;
+import java.util.LinkedList;
 import java.util.List;
+import java.util.Queue;
 
 /**
  * ClassName: M199_BinaryTree_RightSideView
@@ -29,7 +32,41 @@ public class M199_BinaryTree_RightSideView {
         }
     }
 
-    public static List<Integer> rightSideView(TreeNode root) {
-        return null;
+    public List<Integer> rightSideView(TreeNode root) {
+        // 创建空list作为答案储存对象
+        List<Integer> ans = new ArrayList<>();
+        // 如果二叉树为空，返回空列表
+        if(root == null){
+            return ans;
+        }
+        // 如果二叉树不为空，创建队列对二叉树进行层序遍历
+        // 每层的最后一个元素即为二叉树每层的右视图
+        Queue<TreeNode> queue = new LinkedList<>();
+        // 将根节点加入队列
+        queue.offer(root);
+        // 假如队列非空，说明二叉树还有未遍历的元素
+        while(!queue.isEmpty()){
+            // 记录每一层的元素个数
+            int size = queue.size();
+            // 遍历该层的每个元素
+            for(int i = 0; i < size; i++){
+                // 从队列中删除当前元素
+                TreeNode curNode = queue.poll();
+                // 如果遍历到该层最后一个元素
+                if(i == size-1){
+                    // 将其加入存储右视图的答案list中
+                    ans.add(curNode.val);
+                }
+                // 向队列中加入该元素的左孩子和右孩子
+                if(curNode.left != null){
+                    queue.offer(curNode.left);
+                }
+                if(curNode.right != null){
+                    queue.offer(curNode.right);
+                }
+            }
+        }
+        return ans;
+
     }
 }
