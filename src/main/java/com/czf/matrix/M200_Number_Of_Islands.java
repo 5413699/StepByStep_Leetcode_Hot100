aee155d53e1bd7aa078863a0f4d3bcde1b242429
czf <5413699@qq.com>
2026-06-02 22:30:17 +0800

    M200. 岛屿数量
    思路：遍历整个网格，遇到字符 '1' 说明发现一座未统计的新岛屿，答案加一；随后从该位置做 DFS，把上下左右连通的陆地都标记为 '0'，避免后续重复计数。每个格子最多访问一次，时间复杂度 O(mn)，递归栈最坏 O(mn)。
    
    Codex 于 2026-06-02 22:30 +08:00 提交

--- src/main/java/com/czf/matrix/M200_Number_Of_Islands.java
+++ src/main/java/com/czf/matrix/M200_Number_Of_Islands.java
@@ -34,8 +34,39 @@ public class M200_Number_Of_Islands {
     }
 
     // region LeetCode solution
+    private int m;
+    private int n;
+
     public int numIslands(char[][] grid) {
-        return 0;
+        int num = 0;
+        m = grid.length;
+        n = grid[0].length;
+
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if (grid[i][j] == '1') {
+                    num++;
+                    dfs(grid, i, j);
+                }
+            }
+        }
+
+        return num;
+    }
+
+    private void dfs(char[][] grid, int i, int j) {
+        if (i < 0 || i >= m || j < 0 || j >= n) {
+            return;
+        }
+        if (grid[i][j] != '1') {
+            return;
+        }
+
+        grid[i][j] = '0';
+        dfs(grid, i + 1, j);
+        dfs(grid, i - 1, j);
+        dfs(grid, i, j + 1);
+        dfs(grid, i, j - 1);
     }
     // endregion
 }
